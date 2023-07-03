8df384b710da9be9a0096931ace0591190599df2
Sonam <sonammurarkar41@gmail.com>
2023-07-03 16:03:37 +0530

    Jcommerce project initial

---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={HeaderParent}>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+const HeaderParent = {
+
+}
+
+export default Header
